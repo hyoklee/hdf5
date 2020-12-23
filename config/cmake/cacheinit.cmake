cab8b378ca2dfd003612a20d10f0fa57ec00e4d1
H. Joe Lee <hyoklee@hdfgroup.org>
2020-12-23 11:49:10 -0600

    OESS-104:Change SZIP tag name to master.

--- config/cmake/cacheinit.cmake
+++ config/cmake/cacheinit.cmake
@@ -95,7 +95,7 @@ set (ZLIB_PACKAGE_NAME "zlib" CACHE STRING "Name of ZLIB package" FORCE)
 # szip
 #######
 set (SZIP_GIT_URL "https://github.com/hyoklee/szip.git" CACHE STRING "Use SZIP from HDF repo" FORCE)
-set (SZIP_GIT_BRANCH "main" CACHE STRING "" FORCE)
+set (SZIP_GIT_BRANCH "master" CACHE STRING "" FORCE)
 
 
 #######

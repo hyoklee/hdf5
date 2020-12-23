6fbecb7d461d96e49c80ca66923f52b9298e40d1
H. Joe Lee <hyoklee@hdfgroup.org>
2020-12-23 14:46:48 -0600

    OESS-104:Replace SZIP url.

--- config/cmake/cacheinit.cmake
+++ config/cmake/cacheinit.cmake
@@ -94,9 +94,8 @@ set (ZLIB_PACKAGE_NAME "zlib" CACHE STRING "Name of ZLIB package" FORCE)
 #######
 # szip
 #######
-# set (SZIP_GIT_URL "https://github.com/hyoklee/szip.git" CACHE STRING "Use SZIP from HDF repo" FORCE)
-set (SZIP_GIT_URL "https://gitlab.dkrz.de/k202009/libaec.git" CACHE STRING "Use SZIP from HDF repo" FORCE)
-
+# HDF5 1.13 uses libaec instead of szip.
+set (SZIP_GIT_URL "https://github.com/hyoklee/libaec.git" CACHE STRING "Use SZIP from HDF repo" FORCE)
 set (SZIP_GIT_BRANCH "master" CACHE STRING "" FORCE)
 
 

b37eeeedc4ecf184d74390306cde8472223a7da9
hyoklee <hyoklee@users.noreply.github.com>
2023-08-04 20:06:51 +0000

    deploy: 39ffeae3547bab60c4356848edec5caaa9b672f1

---
+++ develop/search/pages_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['about_12216',['About',['../_about.html',1,'']]],
+  ['additional_20resources_12217',['Additional Resources',['../_a_r__u_g.html',1,'']]],
+  ['api_20compatibility_20macros_12218',['API Compatibility Macros',['../api-compat-macros.html',1,'']]]
+];

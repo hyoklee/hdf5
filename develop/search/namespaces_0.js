623478f009c4149abd82905e4c6f93a4fc046775
hyoklee <hyoklee@users.noreply.github.com>
2023-09-30 01:47:30 +0000

    deploy: 14574ab28a84fe9780a9b4b345b7c324a95b616c

--- develop/search/namespaces_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['datasets_6324',['datasets',['../namespaceexamples_1_1datasets.html',1,'examples']]],
-  ['datatypes_6325',['datatypes',['../namespaceexamples_1_1datatypes.html',1,'examples']]],
-  ['groups_6326',['groups',['../namespaceexamples_1_1groups.html',1,'examples']]],
-  ['intro_6327',['intro',['../namespaceexamples_1_1intro.html',1,'examples']]]
-];

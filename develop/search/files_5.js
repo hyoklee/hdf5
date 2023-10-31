4c266aae5ea17a7d9e964b19eb6ac5fa0b8d3a27
hyoklee <hyoklee@users.noreply.github.com>
2023-10-31 01:49:46 +0000

    deploy: 65d08e855bdf56b1401481460611ecd57b3d9040

--- develop/search/files_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['gettingstarted_2edox_6363',['GettingStarted.dox',['../_getting_started_8dox.html',1,'']]],
-  ['glossary_2edox_6364',['Glossary.dox',['../_glossary_8dox.html',1,'']]]
-];
--- develop/search/namespaces_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['type_5fmod_6347',['type_mod',['../namespacetype__mod.html',1,'']]]
-];

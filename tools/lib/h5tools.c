145c50a1dbc6d35720d0517616c0140fbbbcbdff
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2023-11-22 13:50:28 -0600

    fix(h5tools): report error for subfiling vfd if mpi is unavailable
    close #122

--- tools/lib/h5tools.c
+++ tools/lib/h5tools.c
@@ -597,6 +597,9 @@ h5tools_set_fapl_vfd(hid_t fapl_id, h5tools_vfd_info_t *vfd_info)
                     if (H5Pset_fapl_subfiling(fapl_id, (const H5FD_subfiling_config_t *)vfd_info->info) < 0)
                         H5TOOLS_GOTO_ERROR(FAIL, "H5Pset_fapl_subfiling() failed");
                 }
+                else {
+                  H5TOOLS_GOTO_ERROR(FAIL, "MPI is not available for Subfiling VFD");
+                }
 #else
                 H5TOOLS_GOTO_ERROR(FAIL, "The Subfiling VFD is not enabled");
 #endif

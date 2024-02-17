bbcf63d2c069ad204fdb97a8a9bc7e7f1d771f3c
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2024-02-17 17:02:22 -0600

    build: check argument for cmake regex
    close #153

--- config/cmake/HDFUseFortran.cmake
+++ config/cmake/HDFUseFortran.cmake
@@ -37,13 +37,17 @@ FortranCInterface_HEADER (
     SYMBOL_NAMESPACE "H5_FC_"
 )
 
-# file (STRINGS ${CMAKE_BINARY_DIR}/FCMangle.h CONTENTS REGEX "H5_FC_GLOBAL\(.*,.*\) +(.*)")
-# string (REGEX MATCH "H5_FC_GLOBAL\(.*,.*\) +(.*)" RESULT  ${CONTENTS})
-# set (H5_FC_FUNC "H5_FC_FUNC(name,NAME) ${CMAKE_MATCH_1}")
+file (STRINGS ${CMAKE_BINARY_DIR}/FCMangle.h CONTENTS REGEX "H5_FC_GLOBAL\(.*,.*\) +(.*)")
+if (CONTENTS)
+  string (REGEX MATCH "H5_FC_GLOBAL\(.*,.*\) +(.*)" RESULT  ${CONTENTS})
+  set (H5_FC_FUNC "H5_FC_FUNC(name,NAME) ${CMAKE_MATCH_1}")
+endif()
 
-# file (STRINGS ${CMAKE_BINARY_DIR}/FCMangle.h CONTENTS REGEX "H5_FC_GLOBAL_\(.*,.*\) +(.*)")
-# string (REGEX MATCH "H5_FC_GLOBAL_\(.*,.*\) +(.*)" RESULT  ${CONTENTS})
-# set (H5_FC_FUNC_ "H5_FC_FUNC_(name,NAME) ${CMAKE_MATCH_1}")
+file (STRINGS ${CMAKE_BINARY_DIR}/FCMangle.h CONTENTS REGEX "H5_FC_GLOBAL_\(.*,.*\) +(.*)")
+if (CONTENTS)
+  string (REGEX MATCH "H5_FC_GLOBAL_\(.*,.*\) +(.*)" RESULT  ${CONTENTS})
+  set (H5_FC_FUNC_ "H5_FC_FUNC_(name,NAME) ${CMAKE_MATCH_1}")
+endif()
 
 #test code source
 set (SIZEOF_CODE

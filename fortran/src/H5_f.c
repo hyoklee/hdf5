350a4f50d2d211cd6cdcb5020ed765df0d24a877
H. Joe Lee <hyoklee@hdfgroup.org>
2025-04-12 23:00:43 -0500

    fix(flang): disable long double

--- fortran/src/H5_f.c
+++ fortran/src/H5_f.c
@@ -183,7 +183,8 @@ h5init_types_c(hid_t_f *types, hid_t_f *floatingtypes, hid_t_f *integertypes)
     /*
      * FIND H5T_NATIVE_REAL_C_LONG_DOUBLE
      */
-#if H5_FORTRAN_C_LONG_DOUBLE_IS_UNIQUE != 0
+#if 0    
+    /* #if H5_FORTRAN_C_LONG_DOUBLE_IS_UNIQUE != 0 */
     if (sizeof(real_C_LONG_DOUBLE_f) == sizeof(float)) {
         if ((types[13] = (hid_t_f)H5Tcopy(H5T_NATIVE_FLOAT)) < 0)
             return ret_value;
